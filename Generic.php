3224879ba9d53b207ffbd90f05a254bbb510119a
4quila <aquila2163@gmail.com>
2020-08-28 02:38:36 +0430

    Generic class created

---
+++ Generic.php
@@ -0,0 +1,20 @@
+<?php
+
+class Generic
+{
+    const ROW = 'row';
+    const FORM = 'form';
+    const INPUT = 'input';
+    const LABEL = 'label';
+    const ERRORS = 'errors';
+    const TYPE_FORM = 'form';
+    const TYPE_TEXT = 'text';
+    const TYPE_EMAIL = 'email';
+    const TYPE_PASSWORD = 'password';
+    const TYPE_SUBMIT = 'submit';
+    const TYPE_SELECT = 'select';
+    const TYPE_RADIO = 'radio';
+    const TYPE_CHECKBOX = 'checkbox';
+    const DEFAULT_TYPE = self::TYPE_TEXT;
+    const DEFAULT_WRAPPER = 'div';
+}
